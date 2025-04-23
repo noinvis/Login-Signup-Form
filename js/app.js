81a718d1bafa3539da29bacf397b0b34c55b785e
noinvis <lazizbek.sn@gmail.com>
2025-04-23 22:39:06 +0500

    Login form

---
+++ js/app.js
@@ -0,0 +1,99 @@
+{
+    // 1 - task 
+    // let k = 11;
+    // let n = 3;
+    // for (let i = 0; i < n; i++) {
+    //     console.log(k);
+    // }
+}
+
+{
+    // 2 - task 
+    // let a = 3;
+    // let b = 7;
+    // let count = 0;
+    // for (let i = a; i <= b; i++) {
+    //     console.log(i);
+    //     count++;
+    // }
+    // console.log("Sonlar soni:", count + " ta");
+}
+
+{
+    // 3 - task 
+    // let a = 3;
+    // let b = 7;
+    // let count = 0;
+    // for (let i = b - 1; i > a; i--) {
+    //     console.log(i);
+    //     count++;
+    // }
+    // console.log("Sonlar soni:", count + " ta");
+}
+
+{
+    // 4 - task 
+    // let price = 1200; //Masalan
+    // for (let i = 1; i <= 10; i++) {
+    //     console.log(`${i} kg konfet narxi: ${price * i}`);
+    // }
+}
+
+{
+    // 5 - task 
+    // let price = 1500; // 1kg konfet
+    // for (let i = 1; i <= 9; i++) {
+    //     console.log(`${i / 10} kg konfet narxi: ${price * (i / 10)}`);
+    // }
+}
+
+{
+    // 6 - task 
+    // let price = 2000;
+    // for (let i = 12; i <= 20; i += 2) {
+    //     console.log(`${i / 10} kg konfet narxi: ${price * (i / 10)}`);
+    // }
+}
+
+{
+    // 7 - task 
+    // let a = 0;
+    // let b = 10;
+    // let sum = 0;
+    // for (let i = a; i <= b; i++) {
+    //     sum += i;
+    // }
+    // console.log("Yig'indi:", sum);
+}
+
+{
+    // 8 - task 
+    // let a = 1;
+    // let b = 5;
+    // let kopayt = 1;
+    // for (let i = a; i <= b; i++) {
+    //     kopayt *= i;
+    // }
+    // console.log("Ko'paytma:", kopayt);
+}
+
+{
+    // 9 - task 
+    // let a = 1;
+    // let b = 5;
+    // let kv = 0;
+    // for (let i = a; i <= b; i++) {
+    //     kv += i * i;
+    // }
+    // console.log("Kvadratlar yig'indisi:", kv);
+}
+
+{
+    // 10 - task 
+    // let n = 5;
+    // let yig = 0;
+    // for (let i = 1; i <= n; i++) {
+    //     yig += 1 / i;
+    // }
+    // console.log("Yig'indi:", yig);
+}
